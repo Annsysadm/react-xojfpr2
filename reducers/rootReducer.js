dd5070ef2de8b5d33a231d144a380e68fdce1be4
Annsysadm <annsysadm@mail.ru>
2019-09-08 01:28:07 +0300

    combineReducers, 1 file modified

--- reducers/rootReducer.js
+++ reducers/rootReducer.js
@@ -1,5 +1,5 @@
 /* rootReducer нужен для того, чтобы держать все reducers в одном месте*/
-import { combineRedusers } from "redux";
+import { combineReducers } from "redux";
 import { persons } from "./persons";
 
 const rootReducer = combineReducers({
